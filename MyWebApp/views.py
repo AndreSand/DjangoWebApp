1de4bbf8fccdae7e403fe16921b82affcedc3447
Andres Sandoval <android.sjsu@gmail.com>
2015-06-16 15:23:24 -0700

    Users, edit and delete post

--- MyWebApp/views.py
+++ MyWebApp/views.py
@@ -3,6 +3,8 @@ from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -12,6 +14,19 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'MyWebApp/post_detail.html', {'post': post})
 
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('MyWebApp.views.post_detail', pk=pk)
+
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('MyWebApp.views.post_list')
+
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -25,6 +40,7 @@ def post_new(request):
         form = PostForm()
     return render(request, 'MyWebApp/post_edit.html', {'form': form})
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -38,6 +54,13 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'MyWebApp/post_edit.html', {'form': form})
+@login_required
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'MyWebApp/post_draft_list.html', {'posts': posts})
+
+
+
 '''
 #http://stackoverflow.com/questions/3124658/django-delete-object
 def post_edit(request, pk):
